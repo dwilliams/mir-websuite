27e2849f7be919b92bd073db88556249e616d5a6
Daniel Patrick Williams <dwilliams@port8080.net>
2008-12-14 23:02:24 -0700

    Added php file for gadget.

---
+++ htdocs/gadget.php
@@ -0,0 +1,23 @@
+<?php
+  //set base DIR of website to non-root dir
+  $baseDIR = $_SERVER['DOCUMENT_ROOT'] . '/';
+  //set base URL of website to non-root dir
+  $baseURL = 'http://radio.mines.edu';
+  //open database for usage in page
+  include($baseDIR . 'dbopen.inc');
+  
+  //output for the vista gadget
+  //title, artist, album of last played song
+  
+  // Perform SQL query for last played
+  $lpquery = "SELECT title, artist, album FROM lastplayed ORDER BY time DESC LIMIT 1";
+  $lp = pg_query($lpquery) or die('Query failed: ' . pg_last_error());
+  $lprow = pg_fetch_array($lp, 0);
+  echo $lprow["title"] . "\n";
+  echo $lprow["artist"] . "\n";
+  echo $lprow["album"] . "\n";
+  pg_free_result($lp);
+  
+  //tidying up after page
+  include($baseDIR . "dbclose.inc");
+?>
